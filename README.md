913c7c23e4146ef1a769496a7c1c9b41d2dcbda6
Anteste <reversodr@gmail.com>
2020-06-22 09:31:55 +0100

    Feature - Follow bot

---
+++ README.md
@@ -0,0 +1,20 @@
+# GitHub-Follow-Bot
+
+A bot done with python to follow users in GitHub.
+
+
+## How to use:
+
+- Fill your GitHub `username` and `password` in the space provided
+- Fill the person's GitHub `username` whose followers following you want to follow(That might be a little confusing). Basically, select a person(for example, take yourself), you will have some followers, for each of your follower, this bot will follow whoever they are following.
+- Then run the python file (It need selenium to run, so `pip install selenium`).
+
+
+## Why I did this?
+
+Somewhere, I read:
+- One in ten people you follow will follow you back.
+- One in hundred people you follow will star your repos.
+- One in thousand people you follow will fork your repos.
+
+Might be true, might be not 🤷‍.
---
+++ follow_bot.py
@@ -0,0 +1,43 @@
+import time
+import sys
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Initializing the headless chrome
+driver = webdriver.Chrome()
+driver.get("https://github.com/login")
+wait = WebDriverWait(driver, 10)
+
+# Locating username and password field
+username = wait.until(EC.presence_of_element_located((By.ID, "login_field")))
+password = wait.until(EC.presence_of_element_located((By.ID, "password")))
+
+# password and username need to go into these values
+username.send_keys("Username")
+password.send_keys("Password")
+
+# Clicking the sign in button
+login_form = wait.until(EC.presence_of_element_located((By.XPATH, "//input[@value='Sign in']")))
+login_form.click()
+
+# Go to the followers tab
+prepend = ["BaseMax"]
+
+for user in prepend:
+    for t in range(19, 100):
+        string = "https://github.com/{}?page={}&tab=followers".format(user, t)
+        driver.get(string)
+        time.sleep(1)
+
+        follow_button = driver.find_elements_by_xpath("//input[@aria-label='Follow this person']")
+
+        try:
+            for i in follow_button:
+                i.submit()
+        except:
+            pass
+        time.sleep(1)
+
+driver.close()
