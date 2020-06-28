8f869317256a1363bcbc45041a1b7dc64cbac5c0
Anteste <ialami2006@gmail.com>
2020-06-28 18:08:01 +0100

    Fix unfollow -a

--- unfollow_bot.py
+++ unfollow_bot.py
@@ -9,33 +9,54 @@ from datetime import datetime
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--token', help="Your GitHub Personal Access Token", required=True)
 parser.add_argument('-m', '--my-username', help="Your GitHub Username", required=True)
-parser.add_argument('-f', '--file', help="Followers File to Unfollow")
+parser.add_argument('-u', '--user-target', help="User to grab followers from")
+parser.add_argument('-f', '--file', help="unfollow users from a File pre-generated")
 parser.add_argument('-a', '--all', help="Unfollow all your followers", action="store_true")
-parser.add_argument('-mu', '--max-unfollow', help="Max Number of People to Unfollow")
+parser.add_argument('-mf', '--max-followers', help="Max Number of Followers to unfollow")
 args = parser.parse_args()
 
 HEADERS = {"Authorization": "Basic " + b64encode(str(args.my_username + ":" + args.token).encode('utf-8')).decode('utf-8')}
+
+res = requests.get("https://api.github.com/user", headers=HEADERS)
+
+if res.status_code != 200:
+    print("Failure to Authenticate, please check Personal Access Token and Username!")
+    exit(1)
+else:
+    print("Authentication Succeeded!")
 sesh = requests.session()
 sesh.headers.update(HEADERS)
-users_to_unfollow = []
-mnof = args.max_followers
-if args.all:
-    res = sesh.get("https://api.github.com/users/" + args.my_username + "/followers")
+if not args.file:
+
+    target = args.my_username
+    res = sesh.get("https://api.github.com/users/" + target + "/following")
+
+
     links_array = requests.utils.parse_header_links(res.headers['Link'].rstrip('>').replace('>,<', ',<'))
     last_link = links_array[1]['url']
     last_page = last_link.split('=')[-1]
-    print('Grabbing People to Unfollow\nThis may take a while... there are ' + str(last_page) + ' pages to go through.')
+    users_to_follow = []
+    mnof = args.max_followers
+    print('Grabbing People to unfollow\nThis may take a while... there are ' + str(last_page) + ' pages to go through.')
     for page in tqdm.tqdm(range(1, int(last_page)), ncols=35, smoothing=True, bar_format='[PROGRESS] {n_fmt}/{total_fmt} | {bar}'):
-        res = sesh.get('https://api.github.com/users/' + args.my_username + "/followers?limit=100&page=" + str(page)).json()
+        res = sesh.get('https://api.github.com/users/' + target + "/following?limit=100&page=" + str(page)).json()
         for user in res:
-            users_to_unfollow.append(user['login'])
+            users_to_follow.append(user['login'])
         if mnof != None:
-            if len(users_to_unfollow) >= int(mnof):
+            if len(users_to_follow) >= int(mnof):
                 break
-    print("Unfollowing Users... This WILL take a while!")
-    for user in tqdm.tqdm(users_to_unfollow, ncols=35, smoothing=True, bar_format='[PROGRESS] {n_fmt}/{total_fmt} | {bar}'):
+    filename = str(datetime.now().strftime('%m-%d-%YT%H-%M-%S')) + "-" + str(len(users_to_follow)) + "_FOLLOWING.json"
+    with open(filename, 'w+') as f:
+        json.dump(users_to_follow, f, indent=4)
+else:
+    filename = args.file
+
+with open(filename, 'r+') as f:
+    obj = json.load(f)
+    print("Starting to unfollow Users... This WILL Take a while, we must avoid being rate-limited!")
+    for user in tqdm.tqdm(obj, ncols=35, smoothing=True, bar_format='[PROGRESS] {n_fmt}/{total_fmt} | {bar}'):
         while True:
-            time.sleep(5)
+            time.sleep(2)
             res = sesh.delete('https://api.github.com/user/following/' + user)
             if res.status_code != 204:
                 print(res.status_code)
@@ -43,24 +64,3 @@ if args.all:
                 time.sleep(60)
             else:
                 break
-else:
-    if not args.file:
-        print("Please pass a file to unfollow from with -f or use -a to unfollow everybody! Limit with -mu INT")
-    with open(args.file, 'r+') as f:
-        counter = 0
-        obj = json.load(f)
-        print("Unfollowing Users... This WILL take a while!")
-        for user in tqdm.tqdm(obj, ncols=35, smoothing=True, bar_format='[PROGRESS] {n_fmt}/{total_fmt} | {bar}'):
-            if mnof != None:
-                if counter >= int(mnof):
-                    break
-            while True:
-                time.sleep(5)
-                res = sesh.delete('https://api.github.com/user/following/' + user)
-                if res.status_code != 204:
-                    print(res.status_code)
-                    print("We may have been rate-limited, waiting until it stops!")
-                    time.sleep(60)
-                else:
-                    break
-            counter += 1
